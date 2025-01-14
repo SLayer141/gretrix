5dcd071efc402ca86954b099eb7f519f27843224
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-14 11:01:31 +0000

    Add component files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import BookAdd from "@/components/BookAdd";
+import BookList from "@/components/BookList";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+
 function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <BookAdd />
+      <BookList />
+    </div>
+  );
 }
 
 export default Home;
