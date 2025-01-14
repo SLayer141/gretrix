2fcd39056ef8af6bad014a5623100588d94dc8be
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-14 11:21:13 +0000

    add Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,12 @@
+import { CrownIcon } from "lucide-react";
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="flex justify-between border-b border-foreground/50 px-4 py-2">
+      <CrownIcon />
+      <p>P</p>
+    </header>
+  );
 }
 
 export default Header;
